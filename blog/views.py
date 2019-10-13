a41bf9bb348962fb6b32acef51d62c9158174810
Keshvi04 <keshvibhimani499@gmail.com>
2019-10-13 16:48:18 -0700

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
     context={
-        'post': posts
+        'posts': posts
     }
     return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    post =     get_object_or_404(Post,pk=pk)
+    context={
+        'post': post
+    }
+    return render(request, 'blog/post_details.html', context)
